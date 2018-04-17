fbe8baa2ebf55f59c705151678766596a2e6862d
Andy Kim <kimand@oregonstate.edu>
2018-04-17 15:04:31 -0400

    week update: added handlebars, login/register pages

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var express = require('express')
 var app = express()
+var exphbs  = require('express-handlebars');
+
+//configure app
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+	res.render('index')
 })
 
-app.listen(process.env.PORT, function () {
+app.get('/register', function (req, res) {
+	res.render('register')
+})
+
+app.get('/login', function (req, res) {
+	res.render('login')
+})
+
+//start server
+const port = process.env.PORT || 8080;
+app.listen(port, function () {
   console.log('Example app started up!')
 })
 
