23f3917c931fa5c844d7f67df840f79bd02c0ea2
Andy Kim <kimand@oregonstate.edu>
2018-04-17 10:12:38 -0400

    minor edit to index and package.json file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 8000!')
+app.listen(process.env.PORT, function () {
+  console.log('Example app started up!')
 })
 
